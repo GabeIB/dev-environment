70d0a48b2b34c72eacfc65b9eb98df4127250572
GabeIB <gabey300@gmail.com>
2021-03-22 10:18:36 -0700

    update

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ cp .vimrc ~/.vimrc
 cp defaults.vim ~/defaults.vim
 cp .bashrc ~/.bashrc
 cp .bash_aliases ~/.bash_aliases
+cp coc-settings.json ~/.vim/coc-settings.json
 mkdir ~/.backup
 mkdir ~/.backup/undodir
--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ cp ~/defaults.vim defaults.vim
 cp ~/.bashrc .bashrc
 cp ~/.bash_aliases .bash_aliases
 cp ~/.git-prompt.sh .git-prompt.sh
+cp ~/.vim/coc-settings.json coc-settings.json
